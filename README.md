354806d33cd6493eec5169b3c2cc634dbabe2f69
Whitemind109 <ganada38@naver.com>
2023-09-28 19:50:19 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Algorithm_Project
+
+Developed with Unreal Engine 5
---
+++ Source/Algorithm_Project/Algorithm_ProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Algorithm_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALGORITHM_PROJECT_API AAlgorithm_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
