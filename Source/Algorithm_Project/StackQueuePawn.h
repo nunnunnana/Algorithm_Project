1fbf363739005f0a255daaedf4997404471e9391
Whitemind109 <ganada38@naver.com>
2023-11-09 23:14:37 +0900

    5.1에서 5.3으로 버전업, BasicPawn 상속 받는 Pawn 생성

---
+++ Source/Algorithm_Project/StackQueuePawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasicPawn.h"
+#include "StackQueuePawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALGORITHM_PROJECT_API AStackQueuePawn : public ABasicPawn
+{
+	GENERATED_BODY()
+	
+};
