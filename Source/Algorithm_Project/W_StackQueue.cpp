ade9a6b4fc6bdd03621a9242ccc021272b0c99b6
Whitemind109 <ganada38@naver.com>
2023-11-12 22:26:34 +0900

    버튼 기능 추가, 타이머 기능 추가
    1. convertButton 누를 때 마다 Stack Queue 텍스트 변경하도록 함수 추가
    2. SetAlertText 호출 될 때 타이머 설정해서 3초 뒤 alertText Visible 꺼지도록 함수 추가

--- Source/Algorithm_Project/W_StackQueue.cpp
+++ Source/Algorithm_Project/W_StackQueue.cpp
@@ -3,8 +3,38 @@
 
 #include "W_StackQueue.h"
 
+void UW_StackQueue::NativeConstruct()
+{
+	Super::NativeConstruct();
+	// 버튼 클릭시 호출될 델리게이트에 함수를 등록한다
+	convertButton->OnClicked.AddDynamic(this, &UW_StackQueue::SetButtonText);
+	alertText->SetVisibility(ESlateVisibility::Hidden);
+}
 void UW_StackQueue::SetAlertText(FText text)
 {
+	alertText->SetVisibility(ESlateVisibility::Visible);
 	alertText->SetText(text);
-	//MainHUDWidget->GetItemText()->SetText(FText::FromString(alertText));
+
+	// 타이머 초기화
+	GetWorld()->GetTimerManager().ClearTimer(Timer);
+	// 타이머 설정
+	GetWorld()->GetTimerManager().SetTimer(Timer, this, &UW_StackQueue::AdvanceTimer, 1.0f, false, 3.0f);
+
 }
+
+void UW_StackQueue::AdvanceTimer()
+{
+	alertText->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void UW_StackQueue::SetButtonText()
+{
+	if (isQueue == true) {
+		stateText->SetText(FText::FromString("Stack"));
+	}
+	else {
+		stateText->SetText(FText::FromString("Queue"));
+	}
+	isQueue = !isQueue;
+}
+
