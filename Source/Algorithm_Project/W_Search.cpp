5bc1812f7090ec43c8199f41de86df705449702f
Whitemind109 <ganada38@naver.com>
2024-05-20 22:34:59 +0900

    W_Search 위젯 생성
    1. startButton, resetButton 버튼 클릭하면 해당하는 함수 실행하도록 추가
    2. selectedName 변수 추가 후 OnSelectionChanged 이벤트 호출될 때 마다 selectedName 값 변경

--- Source/Algorithm_Project/W_Search.cpp
+++ Source/Algorithm_Project/W_Search.cpp
@@ -7,4 +7,37 @@ void UW_Search::NativeConstruct()
 {
 	Super::NativeConstruct();
 
+	// GetActorOfClass로 월드에 있는 ASortActor 액터 찾기
+	AActor* FoundActor = UGameplayStatics::GetActorOfClass(GetWorld(), ASearch::StaticClass());
+	// ASortActor로 형변환
+	search = Cast<ASearch>(FoundActor);
+	
+	// 버튼 클릭시 호출될 델리게이트에 함수를 등록
+	startButton->OnClicked.AddDynamic(this, &UW_Search::StartSearch);
+	resetButton->OnClicked.AddDynamic(this, &UW_Search::StartReset);
+	searchComboBox->OnSelectionChanged.AddDynamic(this, &UW_Search::SelectedChange);
+}
+
+// 선택한 ComboBox에 맞는 알고리즘 실행
+void UW_Search::StartSearch()
+{
+	if (selectedName == ("BFS"))
+		search->StartBFS();
+	else if (selectedName == ("DFS"))
+		search->StartDFS();
+	else if (selectedName == ("Dijkstra"))
+		search->StartDijkstra();
+	else if (selectedName == ("A*"))
+		search->StartAstar();
+}
+
+void UW_Search::StartReset()
+{
+	search->ResetMap();
+}
+
+// 선택한 정렬 알고리즘 이름 설정
+void UW_Search::SelectedChange(FString selectedItem, ESelectInfo::Type selectionType)
+{
+	selectedName = selectedItem;
 }
--- Source/Algorithm_Project/W_Search.h
+++ Source/Algorithm_Project/W_Search.h
@@ -4,6 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Search.h"
+#include "Components/Button.h"
+#include "Components/ComboBoxString.h"
+#include "Kismet/GameplayStatics.h"
 #include "W_Search.generated.h"
 
 /**
@@ -17,4 +21,29 @@ class ALGORITHM_PROJECT_API UW_Search : public UUserWidget
 public:
 	virtual void NativeConstruct();
 
+private:
+
+	UPROPERTY()
+	FString selectedName = ("Selection Sort");
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* startButton;
+	UPROPERTY(meta = (BindWidget))
+	UButton* resetButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UComboBoxString* searchComboBox;
+
+	UPROPERTY(EditAnyWhere)
+	ASearch* search;
+
+public:
+	UFUNCTION()
+	void StartSearch();
+
+	UFUNCTION()
+	void StartReset();
+
+	UFUNCTION()
+	void SelectedChange(FString selectedItem, ESelectInfo::Type selectionType);
 };
