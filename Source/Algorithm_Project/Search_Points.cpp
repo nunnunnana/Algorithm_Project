bcefdf902c3901872ab2a40b587a7605028f2e51
Whitemind109 <ganada38@naver.com>
2024-05-23 23:50:19 +0900

    Search_Points 액터 생성
    1. Static mesh, material 설정 후 Spawn 하는 액터 생성

---
+++ Source/Algorithm_Project/Search_Points.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Search_Points.h"
+
+// Sets default values
+ASearch_Points::ASearch_Points()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	staticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	staticMesh->SetupAttachment(RootComponent);
+
+	// 일회성 초기화 저장을 위한 구조체
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinder<UStaticMesh> findMesh;
+		FConstructorStatics()
+			: findMesh(TEXT("/Script/Engine.StaticMesh'/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube'"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics;
+	cubeMesh = ConstructorStatics.findMesh.Object;
+
+	// 머티리얼 설정
+	static ConstructorHelpers::FObjectFinder<UMaterialInstance> findMat(TEXT("/Script/Engine.Material'/Game/Asset/Material/M_Base_White.M_Base_White'"));
+	currentMat = findMat.Object;
+
+}
+
+// Called when the game starts or when spawned
+void ASearch_Points::BeginPlay()
+{
+	Super::BeginPlay();
+	FVector currentLocation = this->GetActorLocation();
+	SpawnActor(currentLocation, FVector(0, 0, 1));
+
+}
+
+// Called every frame
+void ASearch_Points::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+// 액터 생성
+void ASearch_Points::SpawnActor(FVector location, FVector color)
+{
+	FRotator rotation(0, 0, 0);
+	FActorSpawnParameters SpawnParams;
+	AStaticMeshActor* target = (AStaticMeshActor*)GetWorld()->SpawnActor<AStaticMeshActor>(AStaticMeshActor::StaticClass(), location, rotation, SpawnParams);
+	// 액터의 위치 이동을 위해 Movable로 설정
+	target->SetMobility(EComponentMobility::Movable);
+	target->GetStaticMeshComponent()->SetStaticMesh(cubeMesh);
+	target->GetStaticMeshComponent()->SetMaterial(0, currentMat);
+}
+
---
+++ Source/Algorithm_Project/Search_Points.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/StaticMeshActor.h"
+#include "Search_Points.generated.h"
+
+UCLASS()
+class ALGORITHM_PROJECT_API ASearch_Points : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASearch_Points();
+
+	UPROPERTY()
+	UStaticMeshComponent* staticMesh;
+
+	UPROPERTY()
+	UStaticMesh* cubeMesh;
+
+	UPROPERTY()
+	UMaterialInstance* currentMat;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+
+public:	
+
+	UFUNCTION()
+	void SpawnActor(FVector location, FVector color);
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
